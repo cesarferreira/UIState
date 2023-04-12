3407e227f54d0f97a72d03c9e0099fca2ef9c468
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2023-04-12 18:25:17 +0100

    removed unused import

--- test/uistate_test.dart
+++ test/uistate_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:uistate/uistate.dart';
+// import 'package:uistate/uistate.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('uistate');
@@ -18,6 +18,6 @@ void main() {
   });
 
   test('getPlatformVersion', () async {
-    // expect(await Uistate.platformVersion, '42');
+    // expect(await UIState.platformVersion, '42');
   });
 }
